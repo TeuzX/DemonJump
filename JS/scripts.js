33d86fe707de38998ff62a3fa7c55f47cc644f4d
Teus <pvphenrique55@gmail.com>
2022-11-07 20:59:10 -0300

    jogo demonjump

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,39 +1,89 @@
-const homem = document.querySelector('.homem')
-const alien = document.querySelector('.alien')
+const Tanjiro = document.querySelector('.Tanjiro');
+const obstaculo = document.querySelector('.obstaculo');
+const score = document.querySelector('.score');
+const highscoreElement = document.querySelector('.Highscore');
+let count = 0;
+let highscore = localStorage.getItem('Highscore:');
+highscoreElement.innerHTML = `Highscore: ${highscore}`;
+
+
+/* PÁGINA INICIAL */
+
+const charOption = document.querySelectorAll('.choose-character .char');
+let charSrc;
+
+function charChoose(id) {
+    charOption[id].addEventListener('click', () => {
+        for(i = 0; i < 3; i++) {
+            charOption[i].style.border = 'none';
+        }
+        charOption[id].style.border = '3px solid #fff';
+
+        if(id == 0) {
+            charSrc = 'Imagens/Tanjiro.gif';
+        } else if(id == 1) {
+            charSrc = 'Imagens/Zenitsu.gif';
+        } else if(id == 2) {
+            charSrc = 'Imagens/Nezuko.gif';
+        }
+        localStorage.setItem('charSrc', charSrc);
+    });
+}
+
+for(i = 0; i < 3; i++) {
+    charChoose(i);
+}
+
+
+Tanjiro.setAttribute('src', localStorage.getItem('charSrc'));
 
 const jump = () => {
-    homem.classList.add('jump');
+    Tanjiro.classList.add('jump');
 
     setTimeout(() => {
 
-        homem.classList.remove('jump');
+        Tanjiro.classList.remove('jump');
 
     }, 500)
 }
 
 const loop = setInterval(() => {
 
-    const alienPosition = alien.offsetLeft;
-    const homemPosition = +window.getComputedStyle(homem).bottom.replace('px', '');
+    const obstaculoPosition = obstaculo.offsetLeft;
+    const TanjiroPosition = +window.getComputedStyle(Tanjiro).bottom.replace('px', '');
 
-    if (alienPosition <= 60 && alienPosition > 0 && homemPosition < 100) {
+    if (obstaculoPosition <= 60 && obstaculoPosition > 0 && TanjiroPosition < 100) {
 
-        alien.style.animation = 'none';
-        alien.style.left = `${alienPosition}px`;
+        obstaculo.style.animation = 'none';
+        obstaculo.style.left = `${obstaculoPosition}px`;
 
-        homem.style.animation = 'none';
-        homem.style.bottom = `${homemPosition}px`;
+        Tanjiro.style.animation = 'none';
+        Tanjiro.style.bottom = `${TanjiroPosition}px`;
 
-        homem.src = './Imagens/morte.gif'
-        homem.style.width = '120px'
-        homem.style.marginLeft = '50px'
+        Tanjiro.src = './Imagens/morte.gif';
+        Tanjiro.style.zIndex = '999';
+        Tanjiro.style.width = '170px';
+        Tanjiro.style.marginLeft = '50px';
+
+        document.querySelector('.opacity').style.display = 'flex';
 
         clearInterval(loop);
-        
+        clearInterval(scored);
     }
 
 }, 10);
 
 
+const scored = setInterval(( )=> {
+    count++;
+    score.innerHTML = `Score: ${count}`;
+    if(count > highscore) {
+        highscore = count;
+        highscoreElement.innerHTML = `Highscore: ${highscore}`;
+        localStorage.setItem('Highscore:', highscore);
+    }
 
-document.addEventListener('keydown', jump)
+}, 150);
+
+
+document.addEventListener('keydown', jump);
